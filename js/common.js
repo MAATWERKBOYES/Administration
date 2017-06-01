a89dbba8d6b8aa727c44cfd980cdcefc11e7ef4d
Lesley Vente <BeeHiveJava@users.noreply.github.com>
2017-06-01 09:24:37 +0200

    Update endpoints, and added basic oauth test.

--- js/common.js
+++ js/common.js
@@ -1,4 +1,5 @@
-var API_HOST_URL = 'http://145.93.97.31:8080/';
+//const API_HOST_URL = 'http://145.93.97.31:8080/';
+const API_HOST_URL = 'http://localhost:63342/Administration/';
 
-var REQUEST_TEACHERS_URL = API_HOST_URL + 'persons';
-var UPDATE_TEACHER_PRESENT_URL = API_HOST_URL + 'person';
+const REQUEST_TEACHERS_URL = API_HOST_URL + 'persons';
+const UPDATE_TEACHER_PRESENT_URL = API_HOST_URL + 'person';
