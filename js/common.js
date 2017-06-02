94fca94870156bc9e5ed317bcd0c6e2d7e35e8b6
Lesley Vente <BeeHiveJava@users.noreply.github.com>
2017-06-02 09:11:03 +0200

    Remove all unneeded files.

--- js/common.js
+++
@@ -1,9 +0,0 @@
-const API_HOST_URL = 'http://145.93.96.194:8080/';
-//const API_HOST_URL = 'http://localhost:63342/Administration/';
-
-const LOGIN_URL = API_HOST_URL + 'login';
-
-const REQUEST_TEACHERS_URL = API_HOST_URL + 'person';
-const UPDATE_TEACHER_PRESENT_URL = API_HOST_URL + 'person/{id}/presence';
-
-const REQUEST_QUESTIONS_URL = API_HOST_URL + 'questions';
